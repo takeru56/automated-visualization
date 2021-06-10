0bf2b013cfd124e1049e20936f68e161a0c69261
TakeruTanaka <takeru.tt5672@gmail.com>
2021-06-11 01:41:28 +0900

    add test

---
+++ visualize/app.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import os
+
+MAKE_RESULT_PATH = lambda f: os.path.join(os.path.dirname(__file__), '../result/' + f + '.png')
+
+def test():
+    left = [1, 2, 3]  # グラフの横軸（X軸）
+    height = [3, 5, 0]  # 値（Y軸）
+    plt.bar(left, height)
+    yyyymm = 202106
+    plt.savefig(MAKE_RESULT_PATH('bid' + str(yyyymm)[4:6]))
+
+
+if __name__ == '__main__':
+    test()
