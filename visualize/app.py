987322a9cc87c153a1a70d7edeaddb3365afdee3
TakeruTanaka <takeru.tt5672@gmail.com>
2021-06-23 01:35:40 +0900

    fix

--- visualize/app.py
+++ visualize/app.py
@@ -7,7 +7,7 @@ def test():
     left = [1, 2, 3]  # グラフの横軸（X軸）
     height = [3, 5, 0]  # 値（Y軸）
     plt.bar(left, height)
-    yyyymm = 202110
+    yyyymm = 202111
     plt.savefig(MAKE_RESULT_PATH('bid' + str(yyyymm)[4:6]))
 
 
